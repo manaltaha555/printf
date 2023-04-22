96f6a574e3bd184c9d0b994e165c0947b7dc5169
manaltaha555 <manaltaha555@gmail.com>
2023-04-22 16:39:02 -0700

    _printf.c updated

---
+++ _printf.c
@@ -0,0 +1,58 @@
+#include"main.h"
+/**
+ *print_buffer - print the contents
+ *@buffer: array
+ *@buffer_index: index
+ *Return: void
+ *
+ */
+void print_buffer(char buffer[], int *buffer_index)
+{
+	if (*buffer_index > 0)
+		write(1, &buffer[0], *buffer_index);
+	*buffer_index = 0;
+}
+/**
+ *_printf - simulation of printf function
+ *@format: the input to the function
+ *Return: number of printed characters
+ *
+ */
+int _printf(const char *format)
+{
+	int j, p = 0, p_chars = 0;
+	int flag, width, precision, size, buffer_index = 0;
+
+	va_list l;
+	char buffer[BUFFER_SIZE];
+
+	if (format == NULL)
+		return (-1);
+	va_start(l, format);
+
+	for (j = 0; format && format[i] != '\0'; i++)
+	{
+		if (format[i] != '%')
+		{
+			buffer[buffer_index++] = format[i];
+			print_buffer(buffer, &buffer_index);
+			p_chars += 1;
+		}
+		else
+		{
+			print_buffer(buffer, &buffer_index);
+			flag = get_flag(format, &j);
+			width = get_width(format, &j, l);
+			precision = get_precision(format, &j, l);
+			size = get_size(format, &j);
+			j++;
+			p = handle_print(format, &j, list, buffer, flag, width, precision, size);
+			if (p == -1)
+				return (-1);
+			p_chars += p;
+		}
+		print_buffer(buffer, &buffer_index);
+		va_end(list);
+		return (p_chars);
+	}
+}
