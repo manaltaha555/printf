c77b7ad8a38d70132d30f233dc5ce2c76785a03d
manaltaha555 <manaltaha555@gmail.com>
2023-04-22 17:48:16 -0700

    fuctions.c

---
+++ functions.c
@@ -0,0 +1,110 @@
+#include"main.h"
+/**
+ *print_char - prints  character
+ *@t: arguments
+ *@buffer: to handle the printf process
+ *@flag: find flags
+ *@w: width of the string
+ *@pre: precision specification
+ *@size: size of specifier
+ *Return: number of characters
+ *
+ */
+int print_char(va_list t, char buffer[], int flag, int w, int pre, int size)
+{
+	char c = va_arg(t, int);
+
+	return (handle_char(c, buffer, flag, width, pre, size));
+}
+/**
+ *print_string - prints the string
+ *@t: arguments
+ *@buffer: to handle the printf process
+ *@flag: find flags
+ *@w: find width
+ *@pre: precision
+ *@size: size of specifier
+ *Return: number of characters
+ */
+int print_string(va_list t, char buffer[], int flag, int w, int pre, int size)
+{
+	int length = 0, i;
+	char *str = va_arg(t, char *);
+
+	UNUSED(buffer);
+	UNUSED(flags);
+	UNUSED(w);
+	UNUSED(pre);
+	UNUSED(size);
+	if (str == NULL)
+	{
+		str = "(null)";
+		if (pre >= 6)
+			str = "      ";
+	}
+	while (str[length] != '\0')
+		length++;
+	if (pre >= 0 && pre < length)
+		length = pre;
+	if (w > length)
+	{
+		if (flag & F_MINUS)
+		{
+			write(1, &str[0], length);
+			for (i = w - length; i > 0; i--)
+				write(1, " ", 1);
+			return (w);
+		}
+		else
+		{
+			for (i = w - length; i > 0; i--)
+				write(1, " ", 1);
+			write(1, &str[0], length);
+			return (w);
+		}
+	}
+	return (write(1, str, length));
+}
+/**
+ *print_binary - prints an unsigned number
+ *@t: arguments
+ *@buffer: to handelling printf process
+ *@flag: find flags
+ *@w: find width
+ *@pre: precision
+ *@size: size of specifiers
+ *Return: number of characters
+ *
+ */
+int print_binary(va_list t, char buffer[], int flag, int w, int pre, int size)
+{
+	unsigned int n, m, j, sum = 0;
+	unsigned int a[32];
+	int count = 0;
+
+	UNUSED(buffer);
+	UNUSED(flag);
+	UNUSED(width);
+	UNUSED(precision);
+	UNUSED(size);
+
+	n = va_arg(t, unsigned int);
+	m = 2147483648;
+	for (j = 1; j < 32; i++)
+	{
+		m /= 2;
+		a[j] = (n / m);
+	}
+	for (i = 0; j < 32; i++)
+	{
+		sum += a[j];
+		if (sum || j == 31)
+		{
+			char y = '0' + a[i];
+
+			write(1, &z, 1);
+			count++;
+		}
+	}
+	return (count);
+}
