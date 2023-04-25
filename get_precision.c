c11d4434ac047c347bd57dd196e0d57f65965d1e
manaltaha555 <manaltaha555@gmail.com>
2023-04-24 17:04:53 -0700

    hshhshsh

---
+++ get_precision.c
@@ -0,0 +1,37 @@
+#include"main.h"
+/**
+ * get_precision - Calculates the precision for printing
+ * @format: Formatted string in which to print the arguments
+ * @i: List of arguments to be printed
+ * @list: list of arguments
+ *
+ * Return: Precision
+ */
+
+int get_precision(const char *format, int *i, va_list list)
+{
+	int cur_i = *i + 1;
+	int precision = -1;
+
+	if (format[cur_i] != '.')
+		return (precision);
+	precision = 0;
+	for (cur_i += 1; format[cur_i] != '\0'; cur_i++)
+	{
+		if (is_digit(format[cur_i]))
+		{
+			precision *= 10;
+			precision += format[cur_i] - '0';
+		}
+		else if (format[cur_i] == '*')
+		{
+			cur_i++;
+			precision = va_arg(list, int);
+			break;
+		}
+		else
+			break;
+	}
+	*i = cur_i - 1;
+	return (precision);
+}
---
+++ get_size.c
@@ -0,0 +1,25 @@
+#include"main.h"
+/**
+ * get_size - find the size to cast the argument
+ * @format: Formatted string
+ * @i: List of arguments
+ * Return: int
+ */
+int get_size(const char *format, int *i)
+{
+	int cur_i = *i + 1;
+	int size = 0;
+
+	if (format[cur_i] == 'l')
+		size = S_LONG;
+	else if (format[cur_i] == 'h')
+		size = S_SHORT;
+	if (size == 0)
+		*i = cur_i - 1;
+	else
+		*i = cur_i;
+	return (size);
+}
+
+
+
